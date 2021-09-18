26708c7e33cb8e090d92f2424c48637084ceae37
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-18 15:21:18 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'rooms/index'
   root to: "rooms#index"
+
+  resources :users, only: [:edit, :update]
 
 end
