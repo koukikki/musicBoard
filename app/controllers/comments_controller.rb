6a57619cc8c487d5078e38711bb695846d7a7eaa
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-22 18:04:13 +0900

    ビューに情報を表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,21 @@ class CommentsController < ApplicationController
   def index
     @comment = Comment.new
     @room = Room.find(params[:room_id])
+    @comments = @room.comments.includes(:user)
   end
 
+  def create
+    @room = Room.find(params[:room_id])
+    @comment = @room.comments.new(comment_params)
+    if @comment.save
+      redirect_to room_comments_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private 
+  def comment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, room_id: params[:room_id])
+  end
 end
