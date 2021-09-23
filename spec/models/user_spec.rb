b5bda8c87721d0a5d0ca17c610d328755d93fc2e
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-23 18:11:28 +0900

    FactoryBotの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'nameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
