2c71f17decf9f12cfd95bcea910ace543dc25e23
k.f.koki <86524836+koukikki@users.noreply.github.com>
2021-09-24 18:50:22 +0900

    jsによるguideの表示

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,19 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+window.addEventListener('load', function(){
+
+  const guideButton = document.getElementById("guide")
+  const text = document.getElementById("text")
+  
+  guideButton.addEventListener('click', function() {
+    if (text.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      text.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      text.setAttribute("style", "display:block;")
+    }
+  })
+})
